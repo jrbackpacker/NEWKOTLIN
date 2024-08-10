6538cbe9f73b778fa8ea47ef88b3797e52b85143
jrbackpacked <161060125+jrbackpacked@users.noreply.github.com>
2024-08-10 19:30:35 +0200

    otro tema del curso

--- app/src/main/java/com/example/newkotlin/Funciones.kt
+++ app/src/main/java/com/example/newkotlin/Funciones.kt
@@ -8,7 +8,6 @@ import android.net.wifi.aware.WifiAwareDataPathSecurityConfig
  **/
 
 
-
 fun main() {
 
     showMyName()
@@ -20,22 +19,22 @@ fun main() {
 
 //lo que hacemos es llamar desde la funcion main a las demas funciones
 
-    fun showMyName(){
+fun showMyName() {
 
-        println("me llamo jesus")
-    }
+    println("me llamo jesus")
+}
 //con current conseguimos agregar los valores en el main sin tener que buscar en la funcion
 
-    fun showMyAge(currentAge: Int){
+fun showMyAge(currentAge: Int) {
 
-        println("tengo $currentAge años"  )
-    }
+    println("tengo $currentAge años")
+}
 
 //la funcion add suma los valores
 
-fun add (firstNumber: Int, secondnumber: Int){
+fun add(firstNumber: Int, secondnumber: Int) {
 
-   println(firstNumber + secondnumber)
+    println(firstNumber + secondnumber)
 
 }
 
---
+++ app/src/main/java/com/example/newkotlin/If_Else.kt
@@ -0,0 +1,102 @@
+package com.example.newkotlin
+
+fun main() {
+
+
+    ifAnidado()
+    ifBasico()
+    ifBoolean()
+    ifInt()
+    ifMultiple()
+    ifMultipleOr()
+
+
+}
+
+
+fun ifMultipleOr() {
+    var pet = "cat"
+    var isHappy = true
+
+    //con el simbolo || (Or) conseguimos otra condicio,
+    // aqui tenemos si pasa esto o lo otro y algo mas.
+
+    if (pet == "dog" || (pet == "cat" && isHappy)) {
+
+        println("Es un perro o un gato")
+    }
+}
+
+fun ifMultiple() {
+
+    var age = 18
+    var parentPermission = true
+    var imHappy = false
+
+    //añadiendo los simbolos && podemos añadir condiciones sin abusar de los if
+
+    if (age >= 18 && parentPermission && imHappy) {
+
+        println("puedo beber cerveza")
+    } else {
+        println("No puedes beber")
+    }
+}
+
+fun ifInt() {
+    var age = 18
+
+    if (age >= 18) {
+
+        println("Puedes beber cerveza")
+    } else {
+
+        println("Te bebes un zumo")
+    }
+}
+
+fun ifBoolean() {
+
+    var soyFeliz: Boolean = true
+
+    //tal y como esta siempre sera true
+    // al poner el signo ! se vuelve lo contrario (false)
+    //if (!soyFeliz) {
+    //        println("Estoy amargado :(")
+    //    }
+
+    if (soyFeliz) {
+        println("Estoy contento")
+
+    }
+}
+
+/**
+ * Con el if anidado podemos simplificar varias condiciones
+ */
+
+fun ifAnidado() {
+
+    val animal = "fish"
+
+    if (animal == "dog") {
+        println("Es un perro")
+    } else if (animal == "cat") {
+        println("Es un gato")
+    } else if (animal == "bird") {
+        println("Es un pajaro")
+    } else {
+        println("No es un animal esperado")
+    }
+}
+
+fun ifBasico() {
+    val name = "Jesus"
+    if (name == "Pepe") {
+        println("El nombre es correcto")
+    } else {
+        println("Este no es Jesus")
+    }
+}
+
+
