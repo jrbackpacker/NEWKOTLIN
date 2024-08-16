15032e1ea2681803b27b1f995db0255bf1ec862e
jrbackpacked <161060125+jrbackpacked@users.noreply.github.com>
2024-08-17 01:32:34 +0200

    otro tema del curso

--- app/src/main/java/com/cursoKotlin/MenuActivity.kt
+++ app/src/main/java/com/cursoKotlin/MenuActivity.kt
@@ -1,14 +1,22 @@
 package com.cursoKotlin
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.cursoKotlin.firstApp.FirstActivity
 import com.example.newkotlin.R
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludAPP)
+        btnSaludApp.setOnClickListener{}
 
+    }
+
+    fun navigateToSaludApp(){
+        val intent = Intent(this, FirstActivity::class.java)
     }
 }
